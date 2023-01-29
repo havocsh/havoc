ddc6ee1527d98e27473dbaf1ba7f12f868c453f2
Tom Daquino <tom@havoc.sh>
2023-01-29 00:29:11 -0800

    Adding -q flag to zip command.

--- havoc_build_packages.sh
+++ havoc_build_packages.sh
@@ -3,19 +3,19 @@
 echo ""
 echo "Building ./HAVOC Lambda deployment packages."
 echo " - packaging havoc_control_api/authorizer"
-cd havoc_control_api/authorizer && zip -r ../../havoc_deploy/aws/terraform/authorizer.zip .
+cd havoc_control_api/authorizer && zip -q -r ../../havoc_deploy/aws/terraform/authorizer.zip .
 cd .. && openssl dgst -sha256 -binary ../havoc_deploy/aws/terraform/authorizer.zip | openssl enc -base64 > ../havoc_deploy/aws/terraform/authorizer.zip.base64sha256
 echo " - packaging havoc_control_api/manage"
-cd manage && zip -r ../../havoc_deploy/aws/terraform/manage.zip .
+cd manage && zip -q -r ../../havoc_deploy/aws/terraform/manage.zip .
 cd .. && openssl dgst -sha256 -binary ../havoc_deploy/aws/terraform/manage.zip | openssl enc -base64 > ../havoc_deploy/aws/terraform/manage.zip.base64sha256
 echo " - packaging havoc_control_api/remote_task"
-cd remote_task && zip -r ../../havoc_deploy/aws/terraform/remote_task.zip .
+cd remote_task && zip -q -r ../../havoc_deploy/aws/terraform/remote_task.zip .
 cd .. && openssl dgst -sha256 -binary ../havoc_deploy/aws/terraform/remote_task.zip | openssl enc -base64 > ../havoc_deploy/aws/terraform/remote_task.zip.base64sha256
 echo " - packaging havoc_control_api/task_control"
-cd task_control && zip -r ../../havoc_deploy/aws/terraform/task_control.zip .
+cd task_control && zip -q -r ../../havoc_deploy/aws/terraform/task_control.zip .
 cd .. && openssl dgst -sha256 -binary ../havoc_deploy/aws/terraform/task_control.zip | openssl enc -base64 > ../havoc_deploy/aws/terraform/task_control.zip.base64sha256
 echo " - packaging havoc_control_api/task_result"
-cd task_result && zip -r ../../havoc_deploy/aws/terraform/task_result.zip .
+cd task_result && zip -q -r ../../havoc_deploy/aws/terraform/task_result.zip .
 cd .. && openssl dgst -sha256 -binary ../havoc_deploy/aws/terraform/task_result.zip | openssl enc -base64 > ../havoc_deploy/aws/terraform/task_result.zip.base64sha256
 cd ..
 echo "Build complete."
