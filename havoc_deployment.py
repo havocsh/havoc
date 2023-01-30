6dc42001e81ae979391b93fa881b6defe38bcef9
Tom Daquino <tom@havoc.sh>
2023-01-30 00:37:23 -0800

    Fixing update errors.

--- havoc_deployment.py
+++ havoc_deployment.py
@@ -21,23 +21,23 @@ class ManageDeployment:
         self.deployment_version = deployment_version
         self.profile = profile
         self.aws_profile = None
-        self.__tf = None
         self.__havoc_client = None
     
     @property
     def havoc_client(self):
-        havoc_profiles = load_havoc_config()
-        if self.profile:
-            api_key = havoc_profiles.get(self.profile, 'API_KEY')
-            secret = havoc_profiles.get(self.profile, 'SECRET')
-            api_region = havoc_profiles.get(self.profile, 'API_REGION')
-            api_domain_name = havoc_profiles.get(self.profile, 'API_DOMAIN_NAME')
-        else:
-            api_key = havoc_profiles.get('default', 'API_KEY')
-            secret = havoc_profiles.get('default', 'SECRET')
-            api_region = havoc_profiles.get('default', 'API_REGION')
-            api_domain_name = havoc_profiles.get('default', 'API_DOMAIN_NAME')
-        self.__havoc_client = havoc.Connect(api_region, api_domain_name, api_key, secret)
+        if self.__havoc_client is None:
+            havoc_profiles = load_havoc_config()
+            if self.profile:
+                api_key = havoc_profiles.get(self.profile, 'API_KEY')
+                secret = havoc_profiles.get(self.profile, 'SECRET')
+                api_region = havoc_profiles.get(self.profile, 'API_REGION')
+                api_domain_name = havoc_profiles.get(self.profile, 'API_DOMAIN_NAME')
+            else:
+                api_key = havoc_profiles.get('default', 'API_KEY')
+                secret = havoc_profiles.get('default', 'SECRET')
+                api_region = havoc_profiles.get('default', 'API_REGION')
+                api_domain_name = havoc_profiles.get('default', 'API_DOMAIN_NAME')
+            self.__havoc_client = havoc.Connect(api_region, api_domain_name, api_key, secret)
         return self.__havoc_client
 
 
@@ -336,6 +336,8 @@ class ManageDeployment:
 
     def update(self):
         # Check for existing deployment
+        no_local_tfstate = None
+        no_remote_tfstate = None
         if not os.path.exists('havoc_deploy/aws/terraform/terraform.tfstate'):
             no_local_tfstate = True
         if not os.path.exists('havoc_deploy/aws/terraform/terraform_backend.tf'):
