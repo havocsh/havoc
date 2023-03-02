ff9951f56b588ffbc3af7904ce87b655984b684e
Tom Daquino <tom@havoc.sh>
2023-03-02 12:09:53 -0800

    Fixing task execution conflict check.

--- havoc_control_api/task_control/execute.py
+++ havoc_control_api/task_control/execute.py
@@ -344,9 +344,8 @@ class Task:
 
         # Verify that the task_name is unique.
         conflict = self.get_task_entry()
-        task_status = conflict['Item']['task_status']['S']
-        if task_status != 'terminated':
-            return format_response(409, 'failed', f'{self.task_name} already exists as a running task', self.log)
+        if 'Item' in conflict:
+            return format_response(409, 'failed', f'{self.task_name} already exists', self.log)
 
         task_host_name = 'None'
         task_domain_name = 'None'
