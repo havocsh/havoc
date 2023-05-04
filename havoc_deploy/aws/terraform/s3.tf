ba67da3b4381710974347470455144a7bcd5e129
Tom Daquino <tom@havoc.sh>
2023-05-04 14:16:08 -0700

    Dropping bucket ACL configs that are redundant since AWS tightened default bucket configurations.

--- havoc_deploy/aws/terraform/s3.tf
+++ havoc_deploy/aws/terraform/s3.tf
@@ -8,38 +8,12 @@ resource "aws_s3_bucket" "playbooks" {
   }
 }
 
-resource "aws_s3_bucket_acl" "playbooks" {
-  bucket = aws_s3_bucket.playbooks.id
-  acl    = "private"
-}
-
-resource "aws_s3_bucket_public_access_block" "playbooks" {
-  bucket                  = aws_s3_bucket.playbooks.id
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
-}
-
 resource "aws_s3_bucket" "playbook_types" {
   bucket = "${var.deployment_name}-playbook-types"
 
   tags = {
     Name = "${var.deployment_name}-playbook-types"
   }
-}
-
-resource "aws_s3_bucket_acl" "playbook_types" {
-  bucket = aws_s3_bucket.playbook_types.id
-  acl    = "private"
-}
-
-resource "aws_s3_bucket_public_access_block" "playbook_types" {
-  bucket                  = aws_s3_bucket.playbook_types.id
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
 }
 
 resource "aws_s3_object" "conti_ransomware_playbook_template" {
@@ -57,30 +31,12 @@ resource "aws_s3_bucket" "workspace" {
   }
 }
 
-resource "aws_s3_bucket_acl" "workspace" {
-  bucket = aws_s3_bucket.workspace.id
-  acl    = "private"
-}
-
-resource "aws_s3_bucket_public_access_block" "workspace" {
-  bucket                  = aws_s3_bucket.workspace.id
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
-}
-
 resource "aws_s3_bucket" "terraform_state" {
   bucket = "${var.deployment_name}-terraform-state"
 
   tags = {
     Name = "${var.deployment_name}-terraform-state"
   }
-}
-
-resource "aws_s3_bucket_acl" "terraform_state" {
-  bucket = aws_s3_bucket.terraform_state.id
-  acl    = "private"
 }
 
 resource "aws_s3_bucket_versioning" "terraform_state" {
@@ -98,12 +54,4 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state" 
       sse_algorithm = "AES256"
     }
   }
-}
-
-resource "aws_s3_bucket_public_access_block" "terraform_state" {
-  bucket                  = aws_s3_bucket.terraform_state.id
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
 }
