45d0fae71fb3a35c725be8425bff403fbdf7867c
Tom Daquino <tom@havoc.sh>
2023-07-06 19:11:10 -0700

    Adding CloudWatch Logs groups with preset retention settings.

--- havoc_deploy/aws/terraform/cw_logs.tf
+++ havoc_deploy/aws/terraform/cw_logs.tf
@@ -1,13 +1,56 @@
 resource "aws_cloudwatch_log_group" "ecs_task_logs" {
-  name = "${var.deployment_name}/tasks_cluster"
+  name              = "${var.deployment_name}/tasks_cluster"
+  retention_in_days = var.results_queue_expiration
 }
 
 resource "aws_cloudwatch_log_group" "ecs_playbook_operator_logs" {
-  name = "${var.deployment_name}/playbook_operator_cluster"
+  name              = "${var.deployment_name}/playbook_operator_cluster"
+  retention_in_days = var.results_queue_expiration
 }
 
 resource "aws_cloudwatch_log_group" "task_results_logging" {
-  name = "${var.deployment_name}/task_results_logging"
+  name              = "${var.deployment_name}/task_results_logging"
+  retention_in_days = var.results_queue_expiration
+}
+
+resource "aws_cloudwatch_log_group" "authorizer" {
+  name              = "/aws/lambda/${var.deployment_name}-authorizer"
+  retention_in_days = var.results_queue_expiration
+}
+
+resource "aws_cloudwatch_log_group" "trigger_executor" {
+  name              = "/aws/lambda/${var.deployment_name}-trigger-executor"
+  retention_in_days = var.results_queue_expiration
+}
+
+resource "aws_cloudwatch_log_group" "manage" {
+  name              = "/aws/lambda/${var.deployment_name}-manage"
+  retention_in_days = var.results_queue_expiration
+}
+
+resource "aws_cloudwatch_log_group" "remote_task" {
+  name              = "/aws/lambda/${var.deployment_name}-remote-task"
+  retention_in_days = var.results_queue_expiration
+}
+
+resource "aws_cloudwatch_log_group" "task_control" {
+  name              = "/aws/lambda/${var.deployment_name}-task-control"
+  retention_in_days = var.results_queue_expiration
+}
+
+resource "aws_cloudwatch_log_group" "playbook_operator_control" {
+  name              = "/aws/lambda/${var.deployment_name}-playbook-operator-control"
+  retention_in_days = var.results_queue_expiration
+}
+
+resource "aws_cloudwatch_log_group" "task_result" {
+  name              = "/aws/lambda/${var.deployment_name}-task-result"
+  retention_in_days = var.results_queue_expiration
+}
+
+resource "aws_cloudwatch_log_group" "playbook_operator_result" {
+  name              = "/aws/lambda/${var.deployment_name}-playbook-operator-result"
+  retention_in_days = var.results_queue_expiration
 }
 
 resource "aws_cloudwatch_log_subscription_filter" "task_result_lambdafunction_logfilter" {
